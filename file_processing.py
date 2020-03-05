2a3428a81fa2923778752c4997206c57c8d229c7
JosefKeller <josefkeller7@gmail.com>
2020-03-06 02:21:41 +0400

    add .csv in gitignore

--- file_processing.py
+++ file_processing.py
@@ -12,7 +12,7 @@ def write_output(frequency, spectral_density, method):
     filename = FILENAME.split('.')[0]
     data = dict(col1=frequency, col2=spectral_density)
     frame = pandas.DataFrame(data)
-    frame.to_csv('%s_%s_out' % (filename, method), index=False, sep='\t', header=False)
+    frame.to_csv('%s_%s_out.csv' % (filename, method), index=False, sep='\t', header=False)
 
 
 x_values, y_values = [], []
