1a137ce056eabe4e9285cbfb815467fbb216ef71
JosefKeller <josefkeller7@gmail.com>
2020-03-23 00:29:57 +0400

    remove csv.reader

--- file_processing.py
+++ file_processing.py
@@ -1,7 +1,5 @@
-import csv
 import os
-import pandas
-import numpy
+import pandas as pd
 import configparser
 
 config = configparser.ConfigParser()
@@ -12,14 +10,9 @@ FILENAME = PATH.split('/')[-1]
 
 
 def write_output(frequency, spectral_density, method, window=None):
-    col1, col2 = [], []
-    for elem in frequency:
-        col1.append(str(elem).replace('.', ','))
-    for elem in spectral_density:
-        col2.append(str(elem).replace('.', ','))
     filename = FILENAME.split('.')[0]
-    data = dict(col1=col1, col2=col2)
-    frame = pandas.DataFrame(data)
+    data = dict(col1=frequency, col2=spectral_density)
+    frame = pd.DataFrame(data)
     if not os.path.exists('output'):
         os.mkdir('output')
     if window is not None:
@@ -28,16 +21,12 @@ def write_output(frequency, spectral_density, method, window=None):
         frame.to_csv('output/%s_%s_out.csv' % (filename, method), index=False, sep='\t', header=False)
 
 
-x_values, y_values = [], []
-
 with open(PATH, 'r') as file:
-    reader = csv.reader(file, delimiter='\t')
-    col_num = len(next(reader))
+    reader = pd.read_csv(file, header=None, delimiter='\t')
+    col_num = reader.shape[1]
     if col_num >= 2:
-        for row in reader:
-            x_values.append(float(row[0].replace(',', '.')))
-            y_values.append(float(row[1].replace(',', '.')))
+        x_values = reader[0][:]
+        y_values = reader[1][:]
     else:
-        for row in reader:
-            y_values.append(float(row[0].replace(',', '.')))
-        x_values = numpy.linspace(0, len(y_values) / 5000., len(y_values))
+        y_values = reader[0][:]
+        x_values = reader.index.values
